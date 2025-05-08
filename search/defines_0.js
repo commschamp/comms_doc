523f5e41081287fe0b56cb9c41d7de5a24640ca8
Alex Robenko <arobenko@gmail.com>
2025-05-09 08:47:33 +1000

    Release v5.4.1

--- search/defines_0.js
+++ search/defines_0.js
@@ -11,5 +11,11 @@ var searchData=
   ['comms_5fmsg_5ftransport_5ffield_5falias_5faccess_5fnotemplate_8',['COMMS_MSG_TRANSPORT_FIELD_ALIAS_ACCESS_NOTEMPLATE',['../Message_8h.html#ad869d496cd7e59771fb13079970cd102',1,'Message.h']]],
   ['comms_5fmsg_5ftransport_5ffield_5falias_5fnotemplate_9',['COMMS_MSG_TRANSPORT_FIELD_ALIAS_NOTEMPLATE',['../Message_8h.html#a6fe96d7272148945853ce61572b3d4b1',1,'Message.h']]],
   ['comms_5fpatch_5fversion_10',['COMMS_PATCH_VERSION',['../version_8h.html#a73b82b0fb29708609422ea2b605a150e',1,'version.h']]],
-  ['comms_5fversion_11',['COMMS_VERSION',['../version_8h.html#a6568e921c9c3e2ec4f3c4ad137c60478',1,'version.h']]]
+  ['comms_5fprotocol_5flayers_5faccess_11',['COMMS_PROTOCOL_LAYERS_ACCESS',['../ProtocolLayerBase_8h.html#a7fb36c91416d5bbfd3434c164cc01692',1,'ProtocolLayerBase.h']]],
+  ['comms_5fprotocol_5flayers_5faccess_5finner_12',['COMMS_PROTOCOL_LAYERS_ACCESS_INNER',['../ProtocolLayerBase_8h.html#ad8fd385995c888a08f3c5c8647a3230f',1,'ProtocolLayerBase.h']]],
+  ['comms_5fprotocol_5flayers_5faccess_5fouter_13',['COMMS_PROTOCOL_LAYERS_ACCESS_OUTER',['../ProtocolLayerBase_8h.html#a394d12f1394bc4c207a4734d75605c87',1,'ProtocolLayerBase.h']]],
+  ['comms_5fprotocol_5flayers_5fnames_14',['COMMS_PROTOCOL_LAYERS_NAMES',['../ProtocolLayerBase_8h.html#a1ad7d81e8c974a2e774db1c5c126c7fa',1,'ProtocolLayerBase.h']]],
+  ['comms_5fprotocol_5flayers_5fnames_5finner_15',['COMMS_PROTOCOL_LAYERS_NAMES_INNER',['../ProtocolLayerBase_8h.html#abf636c5d3c3b9ff161413c5d043cec99',1,'ProtocolLayerBase.h']]],
+  ['comms_5fprotocol_5flayers_5fnames_5fouter_16',['COMMS_PROTOCOL_LAYERS_NAMES_OUTER',['../ProtocolLayerBase_8h.html#a7d4451775274adbd829227dd0f459f70',1,'ProtocolLayerBase.h']]],
+  ['comms_5fversion_17',['COMMS_VERSION',['../version_8h.html#a6568e921c9c3e2ec4f3c4ad137c60478',1,'version.h']]]
 ];
